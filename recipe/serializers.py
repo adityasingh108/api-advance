071dc011d4f56b2aef3c3087fbb8e59e2694337e
aditya kumar singh <65594658+adityasingh108@users.noreply.github.com>
2021-10-20 17:45:47 +0530

    ingredient moel

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -1,5 +1,6 @@
+from django.db import models
 from django.db.models import fields
-from core.models import Tag 
+from core.models import Ingredient, Tag 
 from rest_framework import serializers
 
 
@@ -11,3 +12,12 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id','name',)
         read_only_fields = ('id',)
+        
+        
+class IngredientSerializer(serializers.ModelSerializer):
+    ''' Serializer for Ingredient Oobjects'''
+    class Meta:
+        model = Ingredient
+        fields = ('id','name')
+        read_only_fields = ('id',)
+            
